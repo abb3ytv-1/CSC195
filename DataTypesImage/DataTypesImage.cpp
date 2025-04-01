2f514d3af97ae3f89960259578f6f0ca567bd049
Abbey Kelley <abbeykelley84@gmail.com>
2025-04-01 14:58:28 -0600

    DataTypesImage creted

---
+++ DataTypesImage/DataTypesImage.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool isLightOn = true;
+	double candles = 1.5;
+	string lisenccePlateText = "OUTATIME";
+	int books = 74;
+
+	cout << "Is the light on? " << isLightOn << "\n";
+	cout << "Number of candles: " << candles << "\n";
+	cout << "Lisence plate text: " << lisenccePlateText << "\n";
+	cout << "Number of books: " << books << "\n";
+
+	return 0;
+}
