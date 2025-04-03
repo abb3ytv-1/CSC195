330aa28e6ce188cda85c68d6c78c5b800313d6da
Abbey Kelley <abbeykelley84@gmail.com>
2025-04-03 13:15:04 -0600

    added employee class for payroll program

--- UserDataTypes/Employee.cpp
+++ UserDataTypes/Employee.cpp
@@ -18,3 +18,23 @@ void Employee::Read() {
 	std::cout << "Enter hours worked: ";
 	std::cin >> hoursWorked;
 }
+
+void Employee::Write() {
+	float weeklyGross = wage * hoursWorked;
+	float weeklyTax = weeklyGross * TAX;
+	float weeklyNet = weeklyGross - weeklyTax;
+	float yearlyGross = weeklyGross * 52;
+	float yearlyNet = yearlyGross - (yearlyGross * TAX);
+
+	std::cout << "\nEmployee PayRoll Summary\n";
+	std::cout << "-------------------------\n";
+	std::cout << "Name: " << name << "\n";
+	std::cout << "Age: " << age << "\n";
+	std::cout << "Zipcode: " << zipcode << "\n";
+	std::cout << "Hourly Wage: $" << wage << "\n";
+	std::cout << "Hours Worked: " << hoursWorked << "\n";
+	std::cout << "Weekly Gross Pay: $" << weeklyGross << "\n";
+	std::cout << "Weekly Net Pay: $" << weeklyNet << "\n";
+	std::cout << "Yearly Gross Pay: $" << yearlyGross << "\n";
+	std::cout << "Yearly Net Pay: $" << yearlyNet << "\n";
+}
