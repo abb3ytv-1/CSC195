1c59af9fd00662a3d6a61114d985c3cf8dec6309
Abbey Kelley <abbeykelley84@gmail.com>
2025-04-08 12:15:38 -0600

    Guessing Game conversion

--- GussingGame/GussingGame.cpp
+++ GussingGame/GussingGame.cpp
@@ -1,20 +1,59 @@
-// GussingGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(0));
+
+	int secretNumber = rand() % 100 + 1;
+	int guess;
+	int attempts = 0;
+	const int maxAttempts = 10;
+	bool hasWon = false;
+
+	cout << "Welcome to the Guessing Game!" << endl;
+	cout << "I have selected a number between 1 and 100." << endl;
+	cout << "You have " << maxAttempts << " attempts to guess it." << endl;
+
+	while (attempts < maxAttempts)
+	{
+		cout << "Attempt " << (attempts + 1) << "/" << maxAttempts << ": Enter your guess: ";
+		cin >> guess;
+		attempts++;
+
+		if (guess == secretNumber) {
+			hasWon = true;
+			break;
+		}
+		else if (guess < secretNumber) {
+			cout << "Too low! Try again." << endl;
+		}
+		else {
+			cout << "Too high! Try again." << endl;
+		}
+
+		if (attempts == maxAttempts / 2) {
+			if (secretNumber % 2 == 0) {
+				cout << "Hint: The secret number is even." << endl;
+			}
+			else {
+				cout << "Hint: The secret number is odd." << endl;
+			}
+		}
+
+		cout << endl;
+	}
+
+	if (hasWon) {
+		cout << "Congratulations! You've guessed the number " << secretNumber << "!" << endl;
+		cout << " in " << attempts << " attempts." << endl;
+	}
+	else {
+		cout << "Sorry, you've used all your attempts. The secret number was " << secretNumber << "." << endl;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
