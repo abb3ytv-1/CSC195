56def488253dceaaca02d23276e12eb2ec35ee2b
Abbey Kelley <abbeykelley84@gmail.com>
2025-04-21 18:33:33 -0600

    Memory Review fix

--- MemoryReview/MemoryReview.cpp
+++ MemoryReview/MemoryReview.cpp
@@ -5,56 +5,44 @@ struct Person
 	char name[32];
 	int id;
 };
+
+void Square(int& i) {
+	i = i * i;
+}
+
+void Double(int* i) {
+	*i = *i * 2;
+}
+
+
 int main()
 {
 	// ** REFERENCE **
-	//
-	// declare an int variable and set the value to some number (less than 10)
-	// declare a int reference and set it to the int variable above
-	// output the int variable
-	// set the int reference to some number
-	// output the int variable
-	// what happened to the int variable when the reference was changed? (insert
-	//answer)
-	// output the address of the int variable
-	// output the address of the int reference
-	// are the addresses the same or different? (insert answer)
+	int num = 5;
+	int& ref = num;
+	cout << "num: " << num << endl;
+	ref = 10;
+	cout << "num: " << num << endl;
+	// The int variable's changed to 10 when the refrecne changed.
+
+	cout << "num address: " << &num << endl;
+	cout << "ref address: " << &ref << endl;
+	// The addresses are the same.
+
 	// ** REFERENCE PARAMETER **
-	//
-	// create a function above main() called Square that takes in an int
-	//parameter
-	// in the function, multiply the int parameter by itself and assign it back
-	//to the parameter(i = i * i)
-	// call the Square function with the int variable created in the REFERENCE
-	//section
-	// output the int variable to the console
-	// !! notice how the variable has not changed, this is because we only passed
-	//the value to the function !!
-	// change the Square function to take a int reference
-	// !! notice how the calling variable has now changed, this is because we
-	//'passed by reference' !!
-	// !! when a function takes a reference parameter, any changes to the
-	//parameter changes the calling variable ""
+	Square(num);
+	cout << "num: " << num << endl;
+
 	// ** POINTER VARIABLE **
-	//
-	// declare an int pointer, set it to nullptr (it points to nothing)
-	// set the int pointer to the address of the int variable created in the
-	//REFERENCE section
-	// output the value of the pointer
-	// what is this address that the pointer is pointing to? (insert answer)
-	// output the value of the object the pointer is pointing to (dereference the
-	//pointer)
+	int* ptr = nullptr;
+	ptr = &num;
+	cout << "Pointer Value (address): " << ptr << endl;
+	// This address is the address of the int variable num.
+	cout << "Dereference Pointer Value: " << *ptr << endl;
+
 	// ** POINTER PARAMETER **
-	//
-	// create a function above main() called Double that takes in an int pointer
-	//parameter
-	// in the function, multiply the int pointer parameter by 2 and assign it
-	//back to the parameter(i = i * 2)
-	// !! make sure to dereference the pointer to set the value and not set the
-	//address !!
-	// call the Double function with the pointer created in the POINTER VARIABLE
-	//sections
-	// output the dereference pointer
-	// output the int variable created in the REFERENCE section
-	// did the int variable's value change when using the pointer?
+	Double(ptr);
+	cout << "Derefrenced pointer after Double: " << *ptr << endl;
+	cout << "num after Double: " << num << endl;
+	// Yes, the int variable's value changed when using the pointer.
 }
